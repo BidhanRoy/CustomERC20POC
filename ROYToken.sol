360c6d9d7c2d8d066a47cf8deeb036cf92120ae6
Bidhan Roy <mail2bidhan@gmail.com>
2021-10-17 05:24:59 +0000

    Initial commit

---
+++ ROYToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ROYToken is ERC20 {
+    uint256 constant _maxSupply = 21000000;
+
+    constructor() ERC20("Roy", "ROY") {
+        _mint(msg.sender, _maxSupply);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, 1000);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        _mintMinerReward();
+        super._beforeTokenTransfer(from, to, value);
+    }
+}
